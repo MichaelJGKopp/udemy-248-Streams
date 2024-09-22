a3573f36fe939fdd50f24b62e138bf80eeb6bc64
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-22 19:48:15 +0200

    concat two streams and sout

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -55,12 +55,15 @@ public class Main {
 //    tempStream.forEach(s -> System.out.print(s.toLowerCase() + " ")); // stream already already consumed
 
     String[] strings = {"One", "Two", "Three"};
-    Arrays.stream(strings)
-      .sorted(Comparator.reverseOrder())
-      .forEach(System.out::println);
+    var firstStream = Arrays.stream(strings)
+      .sorted(Comparator.reverseOrder());
+//      .forEach(System.out::println);
 
-    Stream.of("Six", "Five", "Four")
-      .map(String::toUpperCase)
+    var secondStream = Stream.of("Six", "Five", "Four")
+      .map(String::toUpperCase);
+//      .forEach(System.out::println);
+
+    Stream.concat(secondStream, firstStream)
       .forEach(System.out::println);
   }
 }
