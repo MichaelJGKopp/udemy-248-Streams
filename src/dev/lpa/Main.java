0753d0877c05743d7989fde4aff430a4c90a5342
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-22 20:10:03 +0200

    change stream type within intermediate operations

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -64,6 +64,23 @@ public class Main {
 //      .forEach(System.out::println);
 
     Stream.concat(secondStream, firstStream)
+      .map(s -> s.charAt(0) + " - ")
+      .forEach(System.out::println);
+
+    Map<Character, int[]> myMap = new LinkedHashMap<>();
+    int bingoIndex = 1;
+    for (char c : "BINGO".toCharArray()) {
+      int[] numbers = new int[15];
+      int labelNo = bingoIndex;
+      Arrays.setAll(numbers, i -> i + labelNo);
+      myMap.put(c, numbers);
+      bingoIndex += 15;
+    }
+
+    myMap.entrySet()
+      .stream()
+      .map(e -> e.getKey() + " has range: " + e.getValue()[0] + " - " +
+        e.getValue()[e.getValue().length - 1])
       .forEach(System.out::println);
   }
 }
